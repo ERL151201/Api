dc180d45978a16c301ae5313fb146906254a56c7
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-09-14 14:03:02 -0500

    internet conexion

--- js/02-app.js
+++ js/02-app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
     const observer = new IntersectionObserver(entries =>{
+        
         if(entries[0].isIntersecting){
             console.log('Ya esta visible');
         }
